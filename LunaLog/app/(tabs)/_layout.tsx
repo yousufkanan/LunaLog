4b5a28e5f5e7fcac849fadaf244f875d22e9d08f
Ryan Demarest <demarestr@moravian.edu>
2025-10-18 18:03:53 -0400

    feat: rename 'explore' tab to 'History' and remove explore screen

--- LunaLog/app/(tabs)/_layout.tsx
+++ LunaLog/app/(tabs)/_layout.tsx
@@ -27,9 +27,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="History"
         options={{
-          title: "Explore",
+          title: "History",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="paperplane.fill" color={color} />
           ),
