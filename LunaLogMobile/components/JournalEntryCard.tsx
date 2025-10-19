7ff809c79559abe7dba1315991db5bb24d8c01b2
Isaac <45981281+Izesik@users.noreply.github.com>
2025-10-19 10:01:37 -0400

    refactor: remove unused imports in JournalEntryCard component

--- LunaLogMobile/components/JournalEntryCard.tsx
+++ LunaLogMobile/components/JournalEntryCard.tsx
@@ -6,13 +6,9 @@ import {
   Animated,
   RefreshControl,
 } from "react-native";
-import { Collapsible } from "@/components/ui/collapsible";
-import { ExternalLink } from "@/components/external-link";
-import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Fonts } from "@/constants/theme";
 import { Colors } from "@/constants/theme";
 import React, { useEffect, useRef, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
